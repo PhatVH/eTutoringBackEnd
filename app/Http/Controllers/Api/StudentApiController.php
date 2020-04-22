3c37519b1d0cb12690f77b383242260ba360b7f4
PhatVH <phatvhgch16144@fpt.edu.vn>
2020-04-22 12:57:30 +0700

    Update StudentApiController.php

--- app/Http/Controllers/Api/StudentApiController.php
+++ app/Http/Controllers/Api/StudentApiController.php
@@ -192,21 +192,18 @@ class StudentApiController extends Controller
     }
 
     public function getTutor(Request $request){
-        $studentid = Student::where('id', request('student_ID'))->first();
 
-        if($studentid['tutor_ID'] != ''){
-            $tutor = Tutor::where('id', $studentid['tutor_ID'])->get([
-                'id',
-                'user_ID',
-                'tutor_name as name',
-                'tutor_phone as phone',
-                'tutor_email as email'
-            ]);
+        $studentid = Student::where('id', request('student_id'))->first();
 
-            return response()->json($tutor);
-        } else{
-            return response()->json('This student has no tutor');
-        }
+        $tutor = Tutor::where('id', $studentid['tutor_ID'])->get([
+            'id',
+            'user_ID',
+            'tutor_name as name',
+            'tutor_phone as phone',
+            'tutor_email as email'
+        ]);
+
+        return response()->json($tutor);
     }
 
     /**
