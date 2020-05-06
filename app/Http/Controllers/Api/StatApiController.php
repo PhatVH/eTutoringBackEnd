d1a48975f572a4cd69f836d0c29f021737523539
PhatVH <phatvhgch16144@fpt.edu.vn>
2020-05-06 14:02:13 +0700

    Update StatApiController.php

--- app/Http/Controllers/Api/StatApiController.php
+++ app/Http/Controllers/Api/StatApiController.php
@@ -13,15 +13,34 @@ class StatApiController extends Controller
     public function messagesLast7Days()
     {
 
+        $arr = array();
         for ($i = 0; $i < 7; $i++){
             $day = Carbon::now()->subDays($i)->toDateString();
             $chatsThisDay = chatContent::whereBetween('created_at', [$day . ' 00:00:00', $day . ' 23:59:59'])->count();
             $arr[] = $chatsThisDay;
         }
 
+        $arr1 = array();
+        $chats = chatContent::where('created_at', '>', Carbon::now()->subDay(7))->count();
+
+        $arr1[] = $chats;
+
+        $chatsTutor = DB::table('chat_contents')->where('chat_contents.created_at', '>', Carbon::now()->subDay(7))
+            ->join('tutors', 'chat_contents.sender', '=', 'tutors.user_ID')
+            ->count();
+
+        $arr1[] = $chatsTutor;
+
+        $chatsStudent = DB::table('chat_contents')->where('chat_contents.created_at', '>', Carbon::now()->subDay(7))
+            ->join('students', 'chat_contents.sender', '=', 'students.user_ID')
+            ->count();
+
+        $arr1[] = $chatsStudent;
+
         return response()->json([
             'message' => 'success',
-            'numberMess' => array_reverse($arr)
+            'numberMess' => array_reverse($arr),
+            'totalMess' => $arr1
         ]);
     }
 
